3733843587285d87c52d9fe28a85bd6c387335a1
Giovany Lescano <gl12011990@gmail.com>
2025-01-28 08:33:54 -0500

    busquedaporcategorias

--- lib/screens/home/components/categories.dart
+++ lib/screens/home/components/categories.dart
@@ -1,18 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class Categories extends StatelessWidget {
+class Categories extends StatefulWidget {
   const Categories({super.key});
 
   @override
+ _CategoriesState createState() => _CategoriesState();
+}
+
+class _CategoriesState extends State<Categories> {
+  List<Map<String, dynamic>> categories = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchCategories();
+  }
+
+  Future<void> fetchCategories() async {
+    final response = await http.get(Uri.parse('http://localhost:8081/mydealerBackend/public/api/productosTipoCategoria/P1'));
+
+    if (response.statusCode == 200) {
+      final List<dynamic> data = json.decode(response.body);
+      setState(() {
+        categories = data.map((category) {
+          return {
+            "icon": "assets/icons/${category['icon']}.svg", // Ajusta según la estructura de tu API
+            "text": category['name'], // Ajusta según la estructura de tu API
+          };
+        }).toList();
+      });
+    } else {
+      throw Exception('Failed to load categories');
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    List<Map<String, dynamic>> categories = [
-      {"icon": "assets/icons/Flash Icon.svg", "text": "Flash Deal"},
-      {"icon": "assets/icons/Bill Icon.svg", "text": "Bill"},
-      {"icon": "assets/icons/Game Icon.svg", "text": "Game"},
-      {"icon": "assets/icons/Gift Icon.svg", "text": "Daily Gift"},
-      {"icon": "assets/icons/Discover.svg", "text": "More"},
-    ];
     return Padding(
       padding: const EdgeInsets.all(20),
       child: Row(
